4a345b6221e5b8c31e6159dd1e34b6853a1ce23b
droidy12527 <gundiaramasu@gmail.com>
2021-03-21 18:41:03 +0530

    Init model

---
+++ model_train.py
@@ -0,0 +1,120 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.models import model_from_json
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import glob
+import numpy as np
+
+dataset_paths = glob.glob("dataset_characters/**/*.jpg")
+
+cols=4
+rows=3
+fig = plt.figure(figsize=(10,8))
+plt.rcParams.update({"font.size":14})
+grid = gridspec.GridSpec(ncols=cols,nrows=rows,figure=fig)
+np.random.seed(45)
+rand = np.random.randint(0,len(dataset_paths),size=(cols*rows))
+# Plot image
+for i in range(cols*rows):
+    fig.add_subplot(grid[i])
+    image = load_img(dataset_paths[rand[i]])
+    label = dataset_paths[rand[i]].split(os.path.sep)[-2]
+    plt.title('"{:s}"'.format(label))
+    plt.axis(False)
+    plt.imshow(image)
+
+plt.savefig("Visualize_dataset.jpg",dpi=300)    
+# Arange input data and corresponding labels
+X=[]
+labels=[]
+
+for image_path in dataset_paths:
+  label = image_path.split(os.path.sep)[-2]
+  image=load_img(image_path,target_size=(80,80))
+  image=img_to_array(image)
+
+  X.append(image)
+  labels.append(label)
+
+X = np.array(X,dtype="float16")
+labels = np.array(labels)
+
+print("[INFO] Find {:d} images with {:d} classes".format(len(X),len(set(labels))))
+
+
+# perform one-hot encoding on the labels
+lb = LabelEncoder()
+lb.fit(labels)
+labels = lb.transform(labels)
+y = to_categorical(labels)
+
+# save label file so we can use in another script
+np.save('license_character_classes.npy', lb.classes_)
+
+(trainX, testX, trainY, testY) = train_test_split(X, y, test_size=0.10, stratify=y, random_state=42)
+
+# Create our model with pre-trained MobileNetV2 architecture from imagenet
+def create_model(lr=1e-4,decay=1e-4/25, training=False,output_shape=y.shape[1]):
+    baseModel = MobileNetV2(weights="imagenet", 
+                            include_top=False,
+                            input_tensor=Input(shape=(80, 80, 3)))
+
+    headModel = baseModel.output
+    headModel = AveragePooling2D(pool_size=(3, 3))(headModel)
+    headModel = Flatten(name="flatten")(headModel)
+    headModel = Dense(128, activation="relu")(headModel)
+    headModel = Dropout(0.5)(headModel)
+    headModel = Dense(output_shape, activation="softmax")(headModel)
+    
+    model = Model(inputs=baseModel.input, outputs=headModel)
+    
+    if training:
+        # define trainable lalyer
+        for layer in baseModel.layers:
+            layer.trainable = True
+        # compile model
+        optimizer = Adam(lr=lr, decay = decay)
+        model.compile(loss="categorical_crossentropy", optimizer=optimizer,metrics=["accuracy"])    
+        
+    return model
+
+# initilaize initial hyperparameter
+INIT_LR = 1e-4
+EPOCHS = 30
+
+model = create_model(lr=INIT_LR, decay=INIT_LR/EPOCHS,training=True)
+
+BATCH_SIZE = 64
+
+my_checkpointer = [
+                EarlyStopping(monitor='val_loss', patience=5, verbose=1),
+                ModelCheckpoint(filepath="License_character_recognition.h5", verbose=1, save_weights_only=True)
+                ]
+
+result = model.fit(image_gen.flow(trainX, trainY, batch_size=BATCH_SIZE), 
+                   steps_per_epoch=len(trainX) // BATCH_SIZE, 
+                   validation_data=(testX, testY), 
+                   validation_steps=len(testX) // BATCH_SIZE, 
+                   epochs=EPOCHS, callbacks=my_checkpointer)
+
+# save model architectur as json file
+model_json = model.to_json()
+with open("MobileNets_character_recognition.json", "w") as json_file:
+  json_file.write(model_json)
